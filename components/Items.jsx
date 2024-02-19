c8da2b6cd713f23eb9f05391fdcda98a590fe500
yusufdgzl <yusuf.dg13@gmail.com>
2024-02-19 20:23:55 +0300

    Added state - 'showCloseButton' to Items.jsx

--- components/Items.jsx
+++ components/Items.jsx
@@ -7,18 +7,20 @@ export default function Items() {
   const [isVisibleProducts, setIsVisibleProducts] = useState([]);
   const [matchedProducts, setMatchedProducts] = useState([]);
   const [point, setPoint] = useState(0);
-  
-  const [animatePing,setAnimatePing] = useState(false);
+
+  const [animatePing, setAnimatePing] = useState(false);
+  const [showCloseButton, setShowCloseButton] = useState(false);
 
   async function startHandler() {
+
     const response = await fetch("/item-data.json");
     const data = await response.json();
 
-    setMatchedProducts([]);
-
     setProducts(Random(data));
-
+    setMatchedProducts([]);
     setPoint(0);
+
+    setShowCloseButton(true);
   }
 
   useEffect(() => {
@@ -42,7 +44,7 @@ export default function Items() {
         setAnimatePing(true);
 
         setTimeout(() => {
-          setAnimatePing(false)
+          setAnimatePing(false);
         }, 1000);
       }
 
@@ -67,6 +69,8 @@ export default function Items() {
     setIsVisibleProducts([]);
     setMatchedProducts([]);
     setPoint(0);
+
+    setShowCloseButton(false);
   }
 
   return (
@@ -84,32 +88,34 @@ export default function Items() {
       </div>
 
       <div className="flex flex-col justify-between items-center w-[30%] my-20">
-
         <div className="w-3/5 border-2 border-dashed border-[#a62b9460] text-2xl px-6 py-2 text-center text-white rounded-full ">
           <h2 className="flex justify-around items-center relative  ">
             Total point :
             <p className="bg-[#a62b9460] relative flex justify-center items-center w-10 h-10 rounded-full">
               {point}
             </p>
-            {animatePing && <span className="bg-yellow-300 border-2 w-6 h-6 absolute right-2 rounded-full animate-ping"></span>}
+            {animatePing && (
+              <span className="bg-yellow-300 border-2 w-6 h-6 absolute right-2 rounded-full animate-ping"></span>
+            )}
           </h2>
         </div>
 
         <div className="flex w-full justify-around  ">
-
-          <button
-            onClick={startHandler}
-            className=" border-sky-500 border-2 b transition-all text-sky-500 p-4 rounded-lg text-xl font-semibold hover:bg-sky-500 hover:text-white"
-          >
-            Start To Game
-          </button>
-
-          <button
-            onClick={closeHandler}
-            className=" border-red-500 border-2 transition-all text-red-500 p-4 rounded-lg text-xl font-semibold hover:bg-red-500 hover:text-white"
-          >
-            Close To Game
-          </button>
+          {showCloseButton ? (
+            <button
+              onClick={closeHandler}
+              className=" border-red-500 border-2 transition-all text-red-500 p-4 rounded-lg text-xl font-semibold hover:bg-red-500 hover:text-white"
+            >
+              Close To Game
+            </button>
+          ) : (
+            <button
+              onClick={startHandler}
+              className=" border-sky-500 border-2 b transition-all text-sky-500 p-4 rounded-lg text-xl font-semibold hover:bg-sky-500 hover:text-white"
+            >
+              Start To Game
+            </button>
+          )}
         </div>
       </div>
     </>
