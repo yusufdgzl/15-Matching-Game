73e913fdcd0782b81859e2a461182c413ac6d031
yusufdgzl <yusuf.dg13@gmail.com>
2024-02-19 20:09:20 +0300

    Added requirement for card opacity

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 
 export default function ItemCard(props) {
-
+  
   const [cardShadow, setCardShadow] = useState("");
 
   const matched = props.matched;
   const isVisible = props.isVisible;
   const point = props.point;
-  const cardOpacity = matched.includes(props.name) ? "opacity-60" : "";
+  const name = props.name;
+
+  const cardOpacity = matched.includes(name) ? "opacity-60" : "";
+
+  const questionMarkPicture =
+    "https://img.lovepik.com/free-png/20220109/lovepik-gray-question-mark-png-image_401351792_wh860.png";
+  const img = props.isVisible ? props.img : questionMarkPicture;
 
   useEffect(() => {
-    if (matched.includes(props.name)) {
+
+    if (matched.includes(name)) {
       setCardShadow("shadow-[5px_5px_10px_0_#65f44f]");
     } else if (isVisible) {
       setCardShadow("shadow-[0_0_10px_0_yellow]");
@@ -20,17 +27,17 @@ export default function ItemCard(props) {
 
   }, [isVisible, matched]);
 
-  
-
-  const questionMarkPicture =
-    "https://img.lovepik.com/free-png/20220109/lovepik-gray-question-mark-png-image_401351792_wh860.png";
-  const img = props.isVisible ? props.img : questionMarkPicture;
-
   return (
-    <div className={`flex m-auto bg-black rounded-2xl ${point === 60 ? 'animate-wiggle': 'animate-spin-fast-one'}  `}>
+    <div
+      className={`flex m-auto bg-black rounded-2xl 
+      ${point === 60 ? "animate-wiggle" : "animate-spin-fast-one"}`}
+    >
       <button onClick={props.onHandlerShowSelected.bind(null, props.id)}>
+
         <img
-          className={` w-40 h-36 transition-all duration-500 rounded-2xl ${cardOpacity}  ${cardShadow}  ${isVisible && 'scale-105'} `}
+          className={` w-40 h-36 transition-all duration-500 rounded-2xl 
+          ${ point !== 60 && cardOpacity}  ${cardShadow}  ${isVisible && "scale-105"}`}
+
           src={img}
           alt="img"
         />
