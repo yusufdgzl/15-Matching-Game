f2dca942d26f285f2960a82215d09fd920866ffa
yusufdgzl <yusuf.dg13@gmail.com>
2024-02-20 05:28:08 +0300

    added ui 'Congratulations text , play Again button and animation ' to Item.jsx

--- components/Items.jsx
+++ components/Items.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import ItemCard from "./ItemCard";
 import Random from "./helper/Random";
+import AgainIcon from "@/lib/Icons/AgainIcon";
 
 export default function Items() {
   const [products, setProducts] = useState([]);
@@ -12,7 +13,6 @@ export default function Items() {
   const [showCloseButton, setShowCloseButton] = useState(false);
 
   async function startHandler() {
-
     const response = await fetch("/item-data.json");
     const data = await response.json();
 
@@ -88,30 +88,44 @@ export default function Items() {
       </div>
 
       <div className="flex flex-col justify-between items-center w-[30%] my-20">
-        <div className="w-3/5 border-2 border-dashed border-[#a62b9460] text-2xl px-6 py-2 text-center text-white rounded-full ">
-          <h2 className="flex justify-around items-center relative  ">
-            Total point :
-            <p className="bg-[#a62b9460] relative flex justify-center items-center w-10 h-10 rounded-full">
-              {point}
-            </p>
-            {animatePing && (
-              <span className="bg-yellow-300 border-2 w-6 h-6 absolute right-2 rounded-full animate-ping"></span>
-            )}
+        {showCloseButton && (
+          <div className="w-3/5 border-2 border-dashed border-[#a62b9460] text-2xl px-6 py-2 text-center text-white rounded-full ">
+            <h2 className="flex justify-around items-center relative  ">
+              Total point :
+              <p className="bg-[#a62b9460] relative flex justify-center items-center w-10 h-10 rounded-full">
+                {point}
+              </p>
+              {animatePing && (
+                <span className="bg-yellow-300 border-2 w-6 h-6 absolute right-2 rounded-full animate-ping"></span>
+              )}
+            </h2>
+          </div>
+        )}
+
+        {point === 60 && <div className="flex flex-col justify-center items-center ">
+          <h2 className="text-3xl py-2 text-transparent bg-clip-text  bg-gradient-to-r from-indigo-600 via-red-300 to-purple-400 animate-bounce ">
+            Congratulations...
           </h2>
-        </div>
+          <button onClick={startHandler} className="flex w-4/5 justify-around items-center  border-orange-500 border-2 transition-all text-orange-500 px-4 py-2 rounded-lg text-xl font-semibold hover:bg-orange-500 hover:text-white">
+            Play Again
+            <span className=" border-orange-500 border animate-spin rounded-full">
+              <AgainIcon />
+            </span>
+          </button>
+        </div>}
 
         <div className="flex w-full justify-around  ">
           {showCloseButton ? (
             <button
               onClick={closeHandler}
-              className=" border-red-500 border-2 transition-all text-red-500 p-4 rounded-lg text-xl font-semibold hover:bg-red-500 hover:text-white"
+              className=" border-red-500 border-2 transition-all text-red-500 px-4 py-2 rounded-lg text-xl font-semibold hover:bg-red-500 hover:text-white"
             >
               Close To Game
             </button>
           ) : (
             <button
               onClick={startHandler}
-              className=" border-sky-500 border-2 b transition-all text-sky-500 p-4 rounded-lg text-xl font-semibold hover:bg-sky-500 hover:text-white"
+              className=" border-sky-500 border-2 b transition-all text-sky-500 px-4 py-2 rounded-lg text-xl font-semibold hover:bg-sky-500 hover:text-white"
             >
               Start To Game
             </button>
--- components/MatchingArea.jsx
+++ components/MatchingArea.jsx
@@ -2,7 +2,7 @@ import Items from "./Items";
 
 export default function MatchingArea() {
   return (
-      <div className="w-4/5 h-[86%] bg-[#3d1c34] m-auto flex border- border-8 rounded-md  ">
+      <div className="w-4/5 h-[86%] bg-[#3d1c34] m-auto flex  border-8 rounded-md  ">
         <Items />
       </div>
   );
